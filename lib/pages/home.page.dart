8a35579f75c88309b56633eae30a3431456cc471
JCuero-BE <jcuero-oic-be@contractorcampus.com>
2021-09-11 11:33:03 -0500

    inicios diseño

--- lib/pages/home.page.dart
+++ lib/pages/home.page.dart
@@ -1,4 +1,7 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:semanda2flutter1/components/button.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -10,6 +13,8 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
+    double size = MediaQuery.of(context).size.width / 6;
+
     return Scaffold(
       appBar: AppBar(
         title: Text("Home"),
@@ -17,30 +22,109 @@ class _HomePageState extends State<HomePage> {
       body: Column(
         children: [
           Expanded(
-            flex: 5,
+            flex: 2,
             child: Container(
-              child: Text("Zona resultados"),
+              child: Row(
+                children: [Text("Zona resultados")],
+              ),
               color: Colors.blue,
-              height: 300,
             ),
           ),
           Expanded(
             flex: 1,
             child: Container(
-              child: Text("Zona ingreso"),
-              color: Colors.red,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Text(
+                    "0",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 50,
+                    ),
+                  )
+                ],
+              ),
+              color: Color(0xff3C4043),
             ),
           ),
           Expanded(
-            flex: 2,
+            flex: 5,
             child: Container(
-              child: Text("Zona botones"),
-              color: Colors.green,
-              height: 200,
+              //width: double.infinity,
+              // height: double.infinity,
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: _listaBotones1(),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: _listaBotones2(),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: _listaBotones3(),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: _listaBotones4(),
+                  ),
+                ],
+              ),
+              color: Color(0xff202124),
             ),
           )
         ],
       ),
     );
+  }
+
+  _listaBotones1() {
+    return [
+      pintarBoton(metodo: () {}, componente: Text("7")),
+      pintarBoton(metodo: () {}, componente: Text("8")),
+      pintarBoton(metodo: () {}, componente: Text("9")),
+      pintarBoton(metodo: () {}, componente: Text("/")),
+      pintarBoton(metodo: () {}, componente: Text("<-")),
+      pintarBoton(metodo: () {}, componente: Text("C")),
+    ];
+  }
+
+  _listaBotones2() {
+    return [
+      pintarBoton(metodo: () {}, componente: Text("4")),
+      pintarBoton(metodo: () {}, componente: Text("5")),
+      pintarBoton(metodo: () {}, componente: Text("6")),
+      pintarBoton(metodo: () {}, componente: Text("x")),
+      pintarBoton(metodo: () {}, componente: Text("(")),
+      pintarBoton(metodo: () {}, componente: Text(")")),
+    ];
+  }
+
+  _listaBotones3() {
+    return [
+      pintarBoton(metodo: () {}, componente: Text("1")),
+      pintarBoton(metodo: () {}, componente: Text("2")),
+      pintarBoton(metodo: () {}, componente: Text("3")),
+      pintarBoton(metodo: () {}, componente: Text("-")),
+      pintarBoton(metodo: () {}, componente: Text("x²")),
+      pintarBoton(metodo: () {}, componente: Text("√")),
+    ];
+  }
+
+  _listaBotones4() {
+    return [
+      pintarBoton(metodo: () {}, componente: Text("0")),
+      pintarBoton(metodo: () {}, componente: Text(".")),
+      pintarBoton(metodo: () {}, componente: Text("%")),
+      pintarBoton(metodo: () {}, componente: Text("+")),
+      pintarBoton(
+          metodo: () {},
+          componente: Text("="),
+          valorflex: 2,
+          btnColor: Colors.green),
+    ];
   }
 }
